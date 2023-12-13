bd5dde3f5194ef64836c519aa3d5c66784b4273e
keerthi379 <135863582+keerthi379@users.noreply.github.com>
2023-12-13 23:24:35 +0530

    Header.js

---
+++ Header.js
@@ -0,0 +1,15 @@
+// COMPONENT HEADER
+
+function Header()
+{
+    return(
+
+        <div className="header">
+            <h1> List of Items </h1>
+            <p> (One stop solution...) </p>
+        </div>
+    )
+
+}
+
+export default Header;
